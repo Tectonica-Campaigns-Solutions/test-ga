096895c152a87775b02182a91f13a1c6f1426a95
marcelsafont <marcel@tectonica.co>
2025-02-01 16:54:15 +0100

    change plugin

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,17 +11,16 @@ export const onRouteUpdate = ({ location }) => {
   const prevRegion = window.localStorage.getItem("region");
   const region = window.___region || prevRegion || "UK";
 
-  // const cookies = document.cookie.split(";").map((cookie) => cookie.trim());
-  // // const hasAcceptedCookies = cookies.some((cookie) => cookie.startsWith('acceptCookies='));
-  // const hasAcceptedCookies = true;
+  const cookies = document.cookie.split(";").map((cookie) => cookie.trim());
+  // const hasAcceptedCookies = cookies.some((cookie) => cookie.startsWith('acceptCookies='));
+  const hasAcceptedCookies = true;
 
+  // if (window.gtag && region && hasAcceptedCookies) {
+  //   console.log(`Set ${region} for ${location.pathname}`);
 
-    console.log(`Set ${region} for ${location.pathname}`);
-// "G-HPB7SR9GY4",
-    window.gtag("event", "page_view", {
-      page_path: location.pathname,
-      region: region,
-      debug_mode: true
-    });
-  
+  //   window.gtag("event", "page_view", {
+  //     page_path: location.pathname,
+  //     [REGION_DIMENSION]: region,
+  //   });
+  // }
 };
--- gatsby-config.js
+++ gatsby-config.js
@@ -6,36 +6,44 @@ module.exports = {
     title: `test-ga`,
     siteUrl: `https://www.yourdomain.tld`,
   },
+  // plugins: [
+  //   {
+  //     resolve: `gatsby-plugin-google-gtag`,
+  //     options: {
+  //       // You can add multiple tracking ids and a pageview event will be fired for all of them.
+  //       trackingIds: [
+  //         "GTM-K65SG8W9",
+  //         "G-HPB7SR9GY4"
+  //       ],
+  //       // This object gets passed directly to the gtag config command
+  //       // This config will be shared across all trackingIds
+  //       // gtagConfig: {
+  //       //   optimize_id: "OPT_CONTAINER_ID",
+  //       //   anonymize_ip: true,
+  //       //   cookie_expires: 0,
+  //       // },
+  //       // This object is used for configuration specific to this plugin
+  //       // pluginConfig: {
+  //       //   // Puts tracking script in the head instead of the body
+  //       //   head: false,
+  //       //   // Setting this parameter is also optional
+  //       //   respectDNT: true,
+  //       //   // Avoids sending pageview hits from custom paths
+  //       //   exclude: ["/preview/**", "/do-not-track/me/too/"],
+  //       //   // Defaults to https://www.googletagmanager.com
+  //       //   origin: "YOUR_SELF_HOSTED_ORIGIN",
+  //       //   // Delays processing pageview events on route update (in milliseconds)
+  //       //   delayOnRouteUpdate: 0,
+  //       // },
+  //     },
+  //   },
+  // ],
   plugins: [
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: "gatsby-plugin-google-tagmanager",
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "GTM-K65SG8W9",
-          "G-HPB7SR9GY4"
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        // gtagConfig: {
-        //   optimize_id: "OPT_CONTAINER_ID",
-        //   anonymize_ip: true,
-        //   cookie_expires: 0,
-        // },
-        // This object is used for configuration specific to this plugin
-        // pluginConfig: {
-        //   // Puts tracking script in the head instead of the body
-        //   head: false,
-        //   // Setting this parameter is also optional
-        //   respectDNT: true,
-        //   // Avoids sending pageview hits from custom paths
-        //   exclude: ["/preview/**", "/do-not-track/me/too/"],
-        //   // Defaults to https://www.googletagmanager.com
-        //   origin: "YOUR_SELF_HOSTED_ORIGIN",
-        //   // Delays processing pageview events on route update (in milliseconds)
-        //   delayOnRouteUpdate: 0,
-        // },
-      },
-    },
-  ],
+        id: "	GTM-K65SG8W9",
+      }
+    }
+  ]
 };
